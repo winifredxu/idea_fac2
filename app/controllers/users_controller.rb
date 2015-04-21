55bf128056f32c3e9ca34db440cb48ff0f2660b5
Jason <cameljeet@gmail.com>
2015-04-20 17:44:53 -0700

    Added User authentication with Bcrypt -Created sessions controller -Added helper methods in application.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to new_user_path, notice: "Successfully Registered"
+    else
+      flash[:alert] = "Error creating account, please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
